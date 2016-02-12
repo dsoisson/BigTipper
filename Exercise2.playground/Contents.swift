aba8086eb839810051ff5b4748ba4e13b871ca32
Duane Soisson <dsoisson@live.com>
2016-02-11 22:05:24 -0500

    Finished Exercise 2

--- Exercise2.playground/Contents.swift
+++ Exercise2.playground/Contents.swift
@@ -16,7 +16,9 @@
 
 */
 import UIKit
+import Foundation
 
+//My attempt
 var students = (name: "Mathew Sheets", email: "swift.dude@gmail.com")
 var someString = "Other students in my class are Annie, Sam, Jack, Hudson, Oliver."
 var newStudent = "Carson"
@@ -34,5 +36,53 @@ someString.replaceRange(range, with: newStudent.uppercaseString)
 let student1 = someString[someString.startIndex.advancedBy(43)...someString.startIndex.advancedBy(46)]
 let student2 = someString[someString.startIndex.advancedBy(49)...someString.startIndex.advancedBy(54)]
 print("\(student1) and \(student2) are in the class.")
+
+//Attempt following instructor
+
+//created nested tuples
+let allStudents = (
+    (first: "Duane", last: "Soisson", email: "dsoisson@live.com"),
+    (first: "Maria", last: "Soisson", email: "msoisson@email.com"),
+    (first: "Katerini", last: "Soisson", email: "ksoisson@email.com"),
+    (first: "Eleni", last: "Soisson", email: "esoisson@email.com"),
+    (first: "Sophia", last: "Soisson", email: "ssoisson@email.com")
+)
+
+//created constant to represent new student
+let newStudents = (first: "Howard", last: "Soisson", email: "hsoisson@email.com")
+
+//create string with 4 other students from tuple
+var otherStudents =  "Other students in my class are \(allStudents.1.first), \(allStudents.2.first), \(allStudents.3.first), and \(allStudents.4.first)."
+
+//create individual student constants by unpacking tuple
+let firstStudent = allStudents.0
+
+//determine position of the last student in otherStudents so that it can be copied to another separate string
+let lastStudent = allStudents.4 //create string constant with just last student of tuple
+let lastStudentToFind = " and \(lastStudent.first)" //create a the string that needs to be found within the second string (otherStudents)
+let lastStudentRange = otherStudents.rangeOfString(lastStudentToFind)! //use the lastStudentToFind string to match its counterpart in the second string and determine its range
+let lastStudentStrTmp = otherStudents.substringWithRange(lastStudentRange) //use the range found to create the final string to be removed from second string
+let lastStudentStr = lastStudentStrTmp.substringFromIndex(lastStudentStrTmp.startIndex.advancedBy(4)) //make the final string by substring by advancing 4 past the first position to end
+
+let newOtherStudents = otherStudents.substringToIndex(otherStudents.endIndex.advancedBy(-lastStudentToFind.characters.count))
+
+let secondStudent = allStudents.2.first
+let secondStudentRange = otherStudents.rangeOfString(secondStudent)!
+let secondStudentStr = otherStudents.substringWithRange(secondStudentRange)
+
+let thirdStudent = allStudents.3.first
+let thirdStudentRange = otherStudents.rangeOfString(thirdStudent)!
+let thirdStudentStr = otherStudents.substringWithRange(thirdStudentRange)
+
+print("My name is \(allStudents.0.first) \(allStudents.0.last), my email address is \(allStudents.0.email)")
+print(otherStudents)
+print("\(lastStudentStr) dropped this class.")
+print("\(newOtherStudents) and \(newStudents.first.uppercaseString) was added.")
+print("\(secondStudentStr) and \(thirdStudentStr) are in the class.")
+
+
+
+
+
 
 
