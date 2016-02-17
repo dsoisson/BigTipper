a4e72af5d051d6d0ba2d3043eb39440ad2df99e7
Duane Soisson <dsoisson@live.com>
2016-02-16 19:54:09 -0500

    changes

--- Exercise4.playground/Contents.swift
+++ Exercise4.playground/Contents.swift
@@ -22,5 +22,46 @@
 
 import Foundation
 
-var classes: Set<String> = ("Math", "Science", "English", "History")
+var classes: Set<String> = ["Math", "Science", "English", "History"]
+//roster of all students is a list of 
+var students: Set<String> = ["Mathew Sheets", "John Winters", "Sam Smith", "Carson Daily", "Adam Aarons", "Bill Clinton", "George Bush", "Ronald Reagan", "Barack Obama", "Jimmy Carter", "Richard Nixon", "John F Kennedy", "William McKinley", "Abraham Lincoln", "John Adams", "George Washington", "Benjamin Franklin"]
+
+var mathClass: Set<String> = []
+var scienceClass: Set<String> = []
+var englishClass: Set<String> = []
+var historyClass: Set<String> = []
+let classSize = 5
+var spaceAvailable = true
+
+//each class has room for 5 students; 3 students get to take 2 classes
+//create loop to assign student1 to math, student2 to science, student3 to english, student4 to history, student5 to math, until all classes have 5 students and no class has duplicate students
+
+repeat {while spaceAvailable{
+for i in students {
+    switch i {
+    case _ where mathClass.count < classSize:
+        mathClass.insert(i)
+    case _ where scienceClass.count < classSize:
+        scienceClass.insert(i)
+    case _ where englishClass.count < classSize:
+        englishClass.insert(i)
+    case _ where historyClass.count < classSize:
+        historyClass.insert(i)
+    default: break
+    }
+}
+    spaceAvailable = !spaceAvailable
+    
+} 
+
+print(mathClass)
+print(scienceClass)
+print(englishClass)
+print(historyClass)
+
+//create loop to print rosters of each class
+
+
+
+
 
--- todos_1.playground/Contents.swift
+++ todos_1.playground/Contents.swift
@@ -1,6 +1,3 @@
-
-
-import UIKit
 import Foundation
 
 var todos = (
