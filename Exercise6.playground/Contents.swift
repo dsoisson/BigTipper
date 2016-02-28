e7e5b69c4a1163f00a821be60764f02a8ab4fef4
Duane Soisson <dsoisson@live.com>
2016-02-28 16:07:09 -0500

    finished exercise 6

--- Exercise6.playground/Contents.swift
+++ Exercise6.playground/Contents.swift
@@ -22,30 +22,138 @@ let students = [
 ]
 /*:
 **Functions:**
-* all = Returns true if all of the element is not false
+* each = Iterate over each element in the array
+* all = Returns true if all of the elements is not false
 * any = Returns true if at least one of the elements is not false
+* contains = Returns true if the element is present
 * indexOf = Returns the index at which element can be found
-* contains = Returns true if the element is present in the list
-* reject = Returns the elements in the array without the elements that pass a truth test (predicate).
-* filter = Returns an array of all the elements that pass a truth test (predicate).
+* filter = Returns an array of all the elements that pass a truth test
+* reject = Returns the elements in the array without the elements that pass a truth test
 * pluck = Returns an array of a specific value from all the elements
+>> **Constraints:**
+Use the above functions to query your students.
+>>
+>> **Example Output:**
+* Last names of math and sciense students where age > 25 and age < 80
+* ["Kenobi", "Windu", "Solo", "Bacca"]
 */
+
+
 each(students) { (student, index) -> Void in
 
     let first = student["first"]!
     let last = student["last"]!
     
     print("\(first) \(last) is index \(index)")
-}
+} 
+
+//print all the students first and last name and their index.
+//call the function 'each' and send two arguments:
+//(1) argument array of dictionaries type called 'students'.
+//(2) argument Closure. 
+//Are 'student' and 'index' empty variables at this time?  Do they have any values at first?
+//Why does it return 'Void'?
+//In the creation of the string constants 'let first = student["first"], where is 'student' coming from? 
+    //Is this a variable created on the fly? 
+//If the function 'each' returns Void, how is it able to print the names and index of each dictionary?
+//How does the 'index' value increase?
+
+//when calling the function each(<#T##students: [[String : String]]##[[String : String]]#>, closure: <#T##(student: [String : String], index: Int) -> Void#>)
+// Why can we drop the parameter names when calling the function (e.g., 'students' and 'closure'?) 
+    //We can drop 'students' because it is the first parameter and Swift considers it extraneous
+    //'closure' is dropped because it is the last argument and becomes a trailing closure.
+
+each(students) { print("\($0["first"]!) \($0["last"]!) is index \($1)") }
+//$0 represents the first parameter of the closure, 'student' in this case.
+//$0["first"]! fetches from the first parameter of the closure 'student', the value for the key "first", and if present, unwraps it.
+//$0["last"]! fetches from the first parameter of the closure 'student', the value for the key "last", and if present, unwraps it.
+//$1 represents the 2nd parameter value of the closure 'index'.
+//Why doesn't this print the text "Rey Rey is index 16"? 
+    //Is it because the function 'each' returns Void?
+//How do we get it to print "Rey Rey is index 16" then?
+
+//this is an example of using a trailing closure since the last argument in function 'each' is a closure. The closure doesn't have to be inside the ().
+//Why is the output () and not the actual string "Rey Rey is index 16"?
+    //Is it because the function returns 'Void'?
+
 
 print(pluck(students) { (student) -> String in
-    
     return student["age"]!
 })
 
 print(pluck(students) { (student) -> String in
-    
     return student["last"]!
 })
+
+//pluck(<#T##students: [[String : String]]##[[String : String]]#>, closure: <#T##(student: [String : String]) -> String#>)
+//When I call the function 'pluck' it requires 2 parameters: An array of dictionaries [[String:String]]
+//How do I know I do not need to add the parameter name 'students'? 
+    //Error that it is extraneous.  Swift just knows this is the first parameter and doesn't need the name?
+//why do I not need the parameter name 'closure' for the second argument?
+//why do I not need the comma between the first and second parameter?  
+    //Converts to a trailing closure
+//the (student) in the passed Closure is what? 
+    //Is it just referring to the parameter name in the required function type
+
+let test = all(students){$0["first"] != "Duane"}
+if test {
+    print("nobody has a cool name like Duane")
+} else {
+    print("I'm glad to see there is a star wars character named Duane")
+}
+
+all(students){$0["first"] == "Obi-Wan"}
+//returns false because all students first names are not "Obi-Wan"
+
+print("all = \(all(students) { Int($0["age"]!) < 89 })")
+//returns true because all students ages are less than 89.  
+//why doesn't it actually print "all = true"?
+
+print("any = \(any(students) { Int($0["age"]!) <= 16 })")
+//returns true because at least 1 students age is less than or equal to 16
+//why doesn't it print "any = true"?
+
+let test2 = any(students){$0["last"]=="Soisson"}
+//returns false because no student has the last name of Soisson
+
+if test2 {
+    print("I'm glad to see a Soisson in the movies!")
+} else {
+    print("It's unfortunate the Soisson name didn't make it to Star Wars.")
+}
+
+
+
+print("indexOf = \(students[indexOf(students) { $0["last"] == "Sidious" }!]["first"]!)")
+//If indexOf is supposed to return the index number, why is it a bool?
+//Why is it not printing "indexOf = Darth"? see below:
+//The index of the student with last name Sidious is 3
+var info = "class"
+print("index of = \(students[3][info]!)")
+
+print("contains = \(contains(students) { $0["first"] == "Chew" })")
+//the function iterates 12 times before becomes true with the first name of Chew
+//why not printing "contains = true"?
+
+
+print("filter: \n\t\(filter(students) { $0["class"]! == "Engligh" }!)")
+let overHill = filter(students){Int($0["age"]!)==88}!
+print(overHill)
+
+print("reject: \n\t\(reject(students) { Int($0["age"]!) >= 21 }!)")
+
+if let drinkingAge = (reject(students){Int($0["age"]!)>=21}){
+    each(drinkingAge){(student, index) -> Void in
+        let first = student["first"]!
+        print("\(first) cannot drink a beer.")
+    }
+}
+
+
+print("pluck: \n\t\(pluck(students) { $0["age"]! }.sort())")
+
+print("pluck: \n\t\(pluck(students) { $0["last"]! })")
+
+print("Last names of math and science students where age > 25 and age < 80\n\t\(pluck(filter(filter(students) { $0["class"] == "Math" }! + filter(students) { $0["class"] == "Science" }!) { Int($0["age"]!)! > 25 && Int($0["age"]!)! < 80 }!) { $0["last"]! })")
 
 
--- Exercise6.playground/Sources/Functions.swift
+++ Exercise6.playground/Sources/Functions.swift
@@ -1,12 +1,22 @@
 import Foundation
 
-public func iterator(students: [[String:String]], closure: (student: [String:String]) -> Void) {
+public func iterator(students: [[String:String]], closure: (student: [String:String]) -> Void)  {
     
-    for var index = 0; index < students.count; ++index {
+    for var index = 0;
+        index < students.count;
+        ++index {
         
         closure(student: students[index])
+            
     }
 }
+//create a function called iterator with 2 parameters and no return value.
+//1st is named  "students" and has an Array of Dictionary type. Is this why it has the [[ ]]?
+//2nd parameter is named "closure" and has a Closure type. The Closure type accepts a Dictionary and returns nothing. Why doesn't it also have double brackets [[ ]]? It isn't an Array of Dictionaries, just a Dictionary?
+//Do the statements for var index = 0; index < students.count; and ++index belong to 'iterator' or 'closure'?
+//the statements create a new variable integer called 'index'. if the index is less than the count of the array of dictionaries then add 1 to the index and repeat. Does students.count count the number of dictionaries in the array or does it count the number of entries in each dictionary? I think it is a count of each dictionary.
+//What is the purpose of the statement at the end "closure(student: students[index])"?
+
 
 public func each(students: [[String:String]], closure: (student: [String:String], index: Int) -> Void) {
     
@@ -14,6 +24,13 @@ public func each(students: [[String:String]], closure: (student: [String:String]
     
     iterator(students) { closure(student: $0, index: index++) }
 }
+//create a function called 'each' with 2 parameters and no return value
+//The first parameter is called 'students' and has an Array of Dictionaries type.  The name 'students' is independent of the array also called 'students'.
+//The second parameter is called 'closure' and is a Closure Type. The Closure Type accepts 2 parameters of Dictionary and Int and returns nothing (void)
+//The statements create a variable Int called index, calls the function "iterator" using the 2 parameters in 'each' as the arguments.  'students'(array of dictionaries) and the trailing Closure called 'closure'. 
+//The parameter 'student' uses the first String denoted as $0? Is there an actual value associated with this?
+//The parameter 'index' uses index++.  How is this working?  How can index be increased in value at this time? Is there an actual value associated with this?
+
 
 public func pluck(students: [[String:String]], closure: (student: [String:String]) -> String) -> [String] {
     
@@ -29,3 +46,143 @@ public func pluck(students: [[String:String]], closure: (student: [String:String
     
     return plucked
 }
+//the 'pluck' function is meant to take a single string out of the array of dictionaries, add it to another array of strings and return the array.
+//It has 2 parameters and returns an array of strings. 
+//The first parameter is named 'students' and is an Array of Dictionaries type. The 'students' is independent of the array also called students.
+//The second parameter is named 'closure' and is Closure type. The Closure accepts one parameter named "student" and of type Dictionary and returns a String
+//Statements: var plucked = [String]() is initializing an empty array of strings
+
+
+
+// Returns true if all of the elements is not false
+public func all(students: [[String:String]], closure: (student: [String:String]) -> Bool) -> Bool {
+    
+    var all = true
+    
+    iterator(students) { (student) -> Void in
+        
+        if all && !closure(student: student) {
+            
+            all = false
+        }
+    }
+    
+    return all
+}
+//function all has 2 parameters ('students' and 'closure') of type Array of Dictionaries and of type Closure and returns a Bool.
+//The type Closure has 1 parameter ('student') and returns a Bool
+//The statements initialize a bool variable 'all' to true.
+//then calls function 'iterator' with the Array of Dictionaries 'students' as the argument. For each student in students, check 'if all && !closure(student: student)'  --> how does this work?
+
+
+
+// Returns the index at which element can be found
+public func indexOf(students: [[String:String]], closure: (student: [String:String]) -> Bool) -> Int? {
+    
+    var index = -1
+    var found = false
+    
+    iterator(students) { (student) -> Void in
+        
+        if !found {
+            
+            if closure(student: student)  {
+                found = true
+            }
+            
+            index++
+        }
+    }
+    
+    return index == -1 || !found ? nil : index
+}
+
+//function 'indexOf' has 2 parameters ('students', 'closure') of type Array of Dictionaries and of type Function and returns an optional integer.
+//the function type has 1 parameter ('student') and returns a Bool.
+//statements initializes a variable integer called 'index'
+//and another variable bool called 'found'
+//calls the function 'iterator' with argument 'students' and 
+
+
+// Returns true if the element is present
+public func contains(students: [[String:String]], closure: (student: [String:String]) -> Bool) -> Bool {
+    
+    var found = false
+    
+    iterator(students) { (student) -> Void in
+        
+        if !found && closure(student: student) {
+            
+            found = true
+        }
+    }
+    
+    return found
+}
+//function contains accepts 2 parameters and returns a bool named 'found'
+//parameter #1 is named 'students' and is of type Array of Dictionaries.  The Dictionary is of type "String:String"
+//parameter #2 is name 'closure' and is of type Function that accepts one parameter and returns a Bool.
+//statements: initialize a variable boolean named 'found' to false
+//statements: call function 'iterator' to iterate through the Array of Dictionaries
+//the arguments passed to the function iterator the array of dictionaries 'students' and a closure.
+//What is "(student)"? Is it just understood by Swift as one dictionary in the array of dictionaries? similar to 'for name in names' for example?
+//Why is return "Void"?
+//'in' is standard syntax for closures
+//if not false and true make var found = true.  ??Having trouble following the logic here??
+
+
+
+// Returns an array of all the elements that pass a truth test
+public func filter(students: [[String:String]], closure: (student: [String:String]) -> Bool) -> [[String:String]]? {
+    
+    var filter = [[String:String]]()
+    
+    iterator(students) { (student) -> Void in
+        
+        if closure(student: student) {
+            
+            filter.append(student)
+        }
+    }
+    
+    return !filter.isEmpty ? filter : nil
+}
+//function filter has 2 parameters of type Array of Dictionaries and of type Closure and returns an array of dictionaries. 
+//the Closure type has 1 parameter of Dictionary and returns a Bool
+//
+
+
+// Returns the elements in the array without the elements that pass a truth test
+public func reject(students: [[String:String]], closure: (student: [String:String]) -> Bool) -> [[String:String]]? {
+    
+    var keep = [[String:String]]()
+    
+    iterator(students) { (student) -> Void in
+        
+        if !closure(student: student) {
+            
+            keep.append(student)
+        }
+    }
+    
+    return !keep.isEmpty ? keep : nil
+}
+
+// Returns true if at least one of the elements is not false
+public func any(students: [[String:String]], closure: (student: [String:String]) -> Bool) -> Bool {
+    
+    var any = false
+    
+    iterator(students) { (student) -> Void in
+        
+        if !any && closure(student: student) {
+            
+            any = true
+        }
+    }
+    
+    return any
+}
+
+
+
